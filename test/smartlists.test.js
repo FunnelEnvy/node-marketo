83b3515c5cea2966ee71b26c3f831a438844e1b0
Zach Delle <z@z3d.tech>
2019-10-08 17:11:56 -0700

    smartList support call

--- test/smartlists.test.js
+++ test/smartlists.test.js
@@ -6,7 +6,7 @@ describe('SmartLists', function () {
   describe('#find', function () {
   	it('displays list of smart lists', function (done) { 
       marketo.smartList.find({ maxReturn: 200 }).then(function (response) {
-      	assert.equal(response.success, true);
+        assert.equal(response.success, true);
         assert.equal(response.errors.length, 0);
         assert(_.has(response.result[0], 'name'));
         assert(_.has(response.result[0], 'workspace'));
